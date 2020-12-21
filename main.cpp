2b6619f8f5b4cc6c52a2da67eafaff2e982c217c
Daniel Stahl <danny.stahl@gmail.com>
2020-12-21 14:02:36 -0800

    day 1 part 2

--- main.cpp
+++ main.cpp
@@ -27,15 +27,45 @@ int main() {
     int vecSize = myVect.size();
     
     for (int x = 0; x < vecSize; x++) {
-        for (int y = x+1; y < vecSize; y++) {
-            int tempNum = myVect.at(x) + myVect.at(y);
-            
-            if (tempNum == 2020) {
-                num = myVect.at(x) * myVect.at(y);
-                //just exit
+        for (int y = 0; y < vecSize; y++) {
+            for (int z = 0; z < vecSize; z++) {
+                int tempNum = myVect.at(x) + myVect.at(y) + myVect.at(z);
+
+                if (tempNum == 2020) {
+                    num = myVect.at(x) * myVect.at(y) * myVect.at(z);
+                    //just exit
+                }
             }
         }
     }
     
     cout << "total = " << num << endl;
+    
+//    num = 0;
+//    int num2 = 1;
+//    int end = myVect.at(vecSize-num2);
+//
+//
+//    for (int w = 0; w < vecSize - 2 && num != 2020; w++) {
+//        int front = myVect.at(w);
+//
+//        for (int c = w+1; c < (vecSize - num2); c++) {
+//
+//            int num3 = myVect.at(c);
+//
+//            int tempNum = front + end + num3;
+//
+//
+//
+//            if (tempNum == 2020) {
+//                num = front * end * num3;
+//                cout << "total = " << num << endl;
+//            }
+//
+//        }
+//        num2++;
+//
+//    }
+//
+    
 }
